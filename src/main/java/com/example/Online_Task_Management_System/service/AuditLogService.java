a61e8713276b26d3744eb8c35a318f59a848436f
vaibhav parmar <vaibhav@weetechsolution.com>
2025-12-23 14:24:33 +0530

    feat: creating-view-log-feature

--- src/main/java/com/example/Online_Task_Management_System/service/AuditLogService.java
+++ src/main/java/com/example/Online_Task_Management_System/service/AuditLogService.java
@@ -1,8 +1,13 @@
 package com.example.Online_Task_Management_System.service;
 
+import com.example.Online_Task_Management_System.dto.response.PageResponse;
+import com.example.Online_Task_Management_System.entity.AuditLog;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface AuditLogService {
     void log(String action, String description, String entityName, Long entityId);
+
+    ResponseEntity<PageResponse<AuditLog>> viewLogs(int page, int size);
 }
