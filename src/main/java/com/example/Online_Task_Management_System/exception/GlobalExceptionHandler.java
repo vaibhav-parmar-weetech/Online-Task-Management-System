008be4ea748a83ece68f97faa3f72037d621cc11
vaibhav parmar <vaibhav@weetechsolution.com>
2025-12-23 11:55:53 +0530

    fix: resolve login API issue

--- src/main/java/com/example/Online_Task_Management_System/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/Online_Task_Management_System/exception/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.example.Online_Task_Management_System.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,4 +33,29 @@ public class GlobalExceptionHandler {
                         "errors", errors
                 ));
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleInvalidEnum(HttpMessageNotReadableException ex) {
+
+        if (ex.getCause() instanceof InvalidFormatException ife &&
+                ife.getTargetType().isEnum()) {
+
+            return ResponseEntity.badRequest().body(
+                    Map.of(
+                            "status", 400,
+                            "error", "Bad Request",
+                            "message", "Invalid role value. Allowed values: Admin, Manager, Employee"
+                    )
+            );
+        }
+
+        return ResponseEntity.badRequest().body(
+                Map.of(
+                        "status", 400,
+                        "error", "Bad Request",
+                        "message", "Invalid request payload"
+                )
+        );
+    }
+
 }
